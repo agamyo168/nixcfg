581e460025574827276ff00bf3926ff8411e0fdf
Omar Gamal Hamed Agamy <agamyo168@gmail.com>
2025-01-15 13:58:54 +0200

    add nxvim

--- flake.nix
+++ flake.nix
@@ -5,6 +5,12 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 		home-manager.url = "github:nix-community/home-manager/release-24.11";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+       	        nixvim = {
+   	        # url = "github:nix-community/nixvim";
+	        # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+	        url = "github:nix-community/nixvim/nixos-24.11";
+	   	inputs.nixpkgs.follows = "nixpkgs";
+	  	};		
 	};
 	outputs = { self, nixpkgs, home-manager, ... }:
 	let 
