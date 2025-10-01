af0a276a02ecd587f1d4682e9a730b1f7a723f25
agamyo168 <agamyo168@gmail.com>
2025-10-01 04:02:55 +0300

    feat: add shutdown cronjob to local serve

--- hosts/sunny/default.nix
+++ hosts/sunny/default.nix
@@ -9,4 +9,11 @@
     apps.nh.enable = true; # I should make a core module and put all cool modules in it.
     apps.tv.enable = true;
   };
+  # Enable cron jobs
+   services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 0 * * *      root    shutdown now"
+    ];
+  };
 }
